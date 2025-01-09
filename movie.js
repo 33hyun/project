7c964b1c811e9444b0e673be505a22beeba08e12
33hyun <lggx12345@gmail.com>
2025-01-09 15:55:22 +0900

    Add files via upload

--- movie.js
+++ movie.js
@@ -3,6 +3,7 @@ const apiKey = '1e3c00a40110306e1353c4f4ec5d461f'
 const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR&page=1`;
 
 // 인기 영화 목록
+// 에러 처리 적용
 async function fetchPopularMovies() {
   try {
     const response = await fetch(popularMoviesUrl);
@@ -18,19 +19,28 @@ async function fetchPopularMovies() {
 
 
 function displayPopularMovies(movies) {
-  const popularMovieDiv = document.getElementById('popularmovie');
-  popularMovieDiv.innerHTML = ''; // 기존 내용을 초기화
+  const popularMovieDiv = document.querySelector("#popularmovie");
+  popularMovieDiv.innerHTML = '';
 
   movies.forEach(movie => {
     const movieElement = document.createElement('div');
     movieElement.classList.add('movie');
     movieElement.innerHTML = `
+      <img class="moviedetail" src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title}" />
       <h3>${movie.title}</h3>
       <p>평점: ${movie.vote_average}</p>
-      <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title}" />
     `;
     popularMovieDiv.appendChild(movieElement);
   });
 }
+
+// 영화 상세페이지 구현
+// const detail = document.querySelectorAll(".moviedetail");
+// const modal = document.querySelector("#modal");
+// detail.addEventListener("click", function () {
+//   modal.style.display = 'block';
+
+// });
+
 
 document.addEventListener('DOMContentLoaded', fetchPopularMovies);
